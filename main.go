7c0cd832df191dbc6305410e2273863be1a38d49
Snow <523113791@qq.com>
2022-02-21 14:43:28 +0800

    air 自动重载

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+/**
+* init 通常用来
+* 对变量进行初始化
+* 检查/修复程序的状态
+* 注册
+* 运行一次计算
+ */
+func init() {
+
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello snow")
 }
