58a6672491addc2c72e02c4e9dcaafc779ad140b
Snow <523113791@qq.com>
2022-02-21 21:24:19 +0800

    初始化数据库注释

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -19,6 +19,7 @@ func SetupDB() {
 
 	switch config.Get("database.connection") {
 	case "mysql":
+		// 构建dsn信息
 		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?chatset=%v&parseTime=True&multiStatements=true&loc=Local",
 			config.Get("database.mysql.username"),
 			config.Get("database.mysql.password"),
@@ -31,6 +32,7 @@ func SetupDB() {
 			DSN: dsn,
 		})
 	case "sqlite":
+		// 初始化sqlite
 		database := config.Get("database.sqlite.database")
 		dbConfig = sqlite.Open(database)
 	default:
@@ -38,10 +40,13 @@ func SetupDB() {
 
 	}
 
+	// 连接数据库 ，并设置gorm的日志模式
 	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
 
+	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
+	// 设置最大空闲连接数
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
-
+	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
 }
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -8,22 +8,25 @@ import (
 	gormlogger "gorm.io/gorm/logger"
 )
 
+// DB对象
 var DB *gorm.DB
 var SQLDB *sql.DB
 
+// Connect 连接数据库
 func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
+	// 使用gorm.Open 连接数据库
 	var err error
-
 	DB, err = gorm.Open(dbConfig, &gorm.Config{
 		Logger: _logger,
 	})
 
+	// 处理错误
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
+	// 获取底层的sqlDB
 	SQLDB, err = DB.DB()
-
 	if err != nil {
 		fmt.Println(err.Error())
 	}
