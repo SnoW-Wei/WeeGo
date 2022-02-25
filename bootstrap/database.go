c93e1081ecf6498e3d561b72144416f60783715b
Snow <523113791@qq.com>
2022-02-25 15:37:39 +0800

    数据库请求日志

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -7,11 +7,11 @@ import (
 	"weego/app/models/user"
 	"weego/pkg/config"
 	"weego/pkg/database"
+	"weego/pkg/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func SetupDB() {
@@ -42,7 +42,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库 ，并设置gorm的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
---
+++ pkg/logger/gorm_logger.go
@@ -0,0 +1,121 @@
+/*
+ * @Descripttion: talk is cheep , show me the code !
+ * @version: V1.0
+ * @Author: snow.wei
+ * @Date: 2022-02-25 14:51:03
+ * @LastEditors: snow.wei
+ * @LastEditTime: 2022-02-25 15:32:33
+ */
+package logger
+
+import (
+	"context"
+	"errors"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+	"weego/pkg/helpers"
+
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+)
+
+// GormLogger 操作对象，实现 gormlogger.Interface
+type GormLogger struct {
+	ZapLogger     *zap.Logger
+	SlowThreshold time.Duration
+}
+
+// NewGormLogger 外部调用。实例化一个 GormLogger 对象，示例：
+// DB, err := gorm.Open(dbConfig, &gorm.Config{
+// 		Logger: logger,NewGormLogger()
+// })
+func NewGormLogger() GormLogger {
+	return GormLogger{
+		ZapLogger:     Logger,                 // 使用全局的 logger.Logger 对象
+		SlowThreshold: 200 * time.Millisecond, // 慢查询阈值，单位为千分之一秒
+	}
+}
+
+// LogMode 实现 gormlogger.Interface 的 LogMode 方法
+func (l GormLogger) LogMode(level gormlogger.LogLevel) gormlogger.Interface {
+	return GormLogger{
+		ZapLogger:     l.ZapLogger,
+		SlowThreshold: l.SlowThreshold,
+	}
+}
+
+// Info 实现 gormlogger.Interface 的 Info 方法
+func (l GormLogger) Info(ctx context.Context, str string, args ...interface{}) {
+	l.logger().Sugar().Debugf(str, args...)
+}
+
+// Warn 实现 gormlogger.Interface 的 Info 方法
+func (l GormLogger) Warn(ctx context.Context, str string, args ...interface{}) {
+	l.logger().Sugar().Warnf(str, args...)
+}
+
+// Error 实现 gormlogger.Interface 的 Error 方法
+func (l GormLogger) Error(ctx context.Context, str string, args ...interface{}) {
+	l.logger().Sugar().Errorf(str, args...)
+}
+
+func (l GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+
+	// 获取运行时间
+	elapsed := time.Since(begin)
+
+	// 获取 SQL 请求和返回条数
+	sql, rows := fc()
+
+	// 通用字段
+	logFields := []zap.Field{
+		zap.String("sql", sql),
+		zap.String("time", helpers.MicrosecondsStr(elapsed)),
+		zap.Int64("rows", rows),
+	}
+
+	// Gorm 错误
+	if err != nil {
+		// 记录未找到的错误使用 waring 等级
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			l.logger().Warn("Database ErrRecordNotFound", logFields...)
+		} else {
+			// 其他错误使用 error 等级
+			logFields = append(logFields, zap.Error(err))
+			l.logger().Error("Database Error", logFields...)
+		}
+	}
+
+	if l.SlowThreshold != 0 && elapsed > l.SlowThreshold {
+		l.logger().Warn("Database Slow Log", logFields...)
+	}
+
+	// 记录所有 SQL 请求
+	l.logger().Debug("Database Query", logFields...)
+}
+
+func (l GormLogger) logger() *zap.Logger {
+
+	var (
+		gormPackage    = filepath.Join("gorm.io", "gorm")
+		zapgormPackage = filepath.Join("moul.io", "zapgorm2")
+	)
+
+	clone := l.ZapLogger.WithOptions(zap.AddCallerSkip(-2))
+
+	for i := 2; i < 15; i++ {
+		_, file, _, ok := runtime.Caller(i)
+		switch {
+		case !ok:
+		case strings.HasSuffix(file, "_test.go"):
+		case strings.Contains(file, gormPackage):
+		case strings.Contains(file, zapgormPackage):
+		default:
+			return clone.WithOptions(zap.AddCallerSkip(i))
+		}
+	}
+	return l.ZapLogger
+}
