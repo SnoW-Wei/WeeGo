2b256d65b9ca673b01ee36523b3d8e59a2e21ae7
Snow <523113791@qq.com>
2022-03-11 13:39:23 +0800

    Guest 中间件

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: talk is cheep , show me the code !
+ * @version: V1.0
+ * @Author: snow.wei
+ * @Date: 2022-02-21 20:46:28
+ * @LastEditors: snow.wei
+ * @LastEditTime: 2022-03-07 00:11:01
+ */
 package database
 
 import (
@@ -13,6 +21,7 @@ var DB *gorm.DB
 var SQLDB *sql.DB
 
 // Connect 连接数据库
+// TODO 支持多库连接
 func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
 	// 使用gorm.Open 连接数据库
 	var err error
