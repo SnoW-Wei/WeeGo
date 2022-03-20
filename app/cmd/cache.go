77fe89f738030d9d48a0f69871d95bbc79a79924
Snow <523113791@qq.com>
2022-03-20 12:28:11 +0800

    cache forget command

--- app/cmd/cache.go
+++ app/cmd/cache.go
@@ -4,11 +4,12 @@
  * @Author: snow.wei
  * @Date: 2022-03-20 10:56:39
  * @LastEditors: snow.wei
- * @LastEditTime: 2022-03-20 10:59:03
+ * @LastEditTime: 2022-03-20 12:27:41
  */
 package cmd
 
 import (
+	"fmt"
 	"weego/pkg/cache"
 	"weego/pkg/console"
 
@@ -26,12 +27,31 @@ var CmdCacheClear = &cobra.Command{
 	Run:   runCacheClear,
 }
 
+var CmdCacheForget = &cobra.Command{
+	Use:   "forget",
+	Short: "Delete Redis key , example: cache forget cache-key",
+	Run:   runCacheForget,
+}
+
+// forget 命令的选项
+var cacheKey string
+
 func init() {
-	CmdCache.AddCommand(CmdCacheClear)
+	// 注册cache 命令的子命令
+	CmdCache.AddCommand(CmdCacheClear, CmdCacheForget)
+
+	// 设置 cache forget 命令的选项
+	CmdCacheForget.Flags().StringVarP(&cacheKey, "key", "k", "", "KEY of the cache")
+	CmdCacheForget.MarkFlagRequired("key")
 }
 
 func runCacheClear(cmd *cobra.Command, args []string) {
 
 	cache.Flush()
 	console.Success("cache cleard.")
+}
+
+func runCacheForget(cmd *cobra.Command, args []string) {
+	cache.Forget(cacheKey)
+	console.Success(fmt.Sprintf("cache key [%s] deleted", cacheKey))
 }
